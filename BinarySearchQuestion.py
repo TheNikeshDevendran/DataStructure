c55f7558eee4de241a6f42aeca3eca10dc17e24a
nikesh <nikeshdevendran07@gmail.com>
2026-02-18 08:51:59 +0530

    Solved Binary Search Question

--- BinarySearchQuestion.py
+++ BinarySearchQuestion.py
@@ -23,4 +23,46 @@ def BinarySearch(arr,target):
     else:
         print(f'The value {target} is not present \nindex{-1}')
 
-BinarySearch(num,600)
+# BinarySearch(num,600)
+
+
+
+
+''' Question: Take a sorted arary and find the start and end position of 
+a target value
+ex:[5,7,7,10,11]
+target=7
+Start_end_positon[1,2]
+''' 
+arr=[5,6,7,7,7,8,10]
+target=0
+start_end_position=[-1,-1]
+low=0
+high=len(arr)-1
+while(low<=high):
+    mid=(high+low)//2
+    if target==arr[mid]:
+        start_end_position[0]=mid
+        high=mid-1
+
+    elif target>arr[mid]:
+        low=mid+1
+    else:
+        high=mid-1
+
+# -----------------------------------
+
+low=0
+high=len(arr)-1
+while(low<=high):
+    mid=(high+low)//2
+    if target==arr[mid]:
+        start_end_position[1]=mid
+        low=mid+1
+
+    elif target>arr[mid]:
+        low=mid+1
+    else:
+        high=mid-1
+
+print(start_end_position)
