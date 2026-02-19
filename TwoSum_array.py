4c41dbc541771ea03c194c44b1b534aebd932980
nikesh <nikeshdevendran07@gmail.com>
2026-02-19 06:36:20 +0530

    Working on hashmap to find two sum pair to achieve traget value

--- TwoSum_array.py
+++ TwoSum_array.py
@@ -8,7 +8,10 @@ pairs:[2,3] [4,1]
 but we need to give only one pair
 and solve it by time complexity O(n)^2
 '''
-
+'''
+in Dictionary what ever operation we are performing it will take only
+O(1)
+'''
 # method 1...
 arr=[2,1,5,6,3,3,4]
 target=6
@@ -21,4 +24,18 @@ for i in range(0,len(arr)-1):
 
 # method 2...
 
+# using hashmap
+def two_sum_all_pairs(arr, target):
+    seen = {}         
+    result = []       
+    for i, num in enumerate(arr):
+        needed = target - num
+        if needed in seen:
+            result.append((seen[needed], i))
+        seen[num] = i
+    return result
 
+arr = [2,4,5,1,0,7]
+target = 6
+
+print(two_sum_all_pairs(arr, target))
